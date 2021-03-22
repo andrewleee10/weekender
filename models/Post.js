b8b774fe19dffe59508e9a7708e93258e101d4c1
Smeza314 <sebastianmeza314@gmail.com>
2021-03-22 11:59:42 -0700

    a

--- models/Post.js
+++ models/Post.js
@@ -2,11 +2,11 @@ const pls = require('passport-local-sequelize')
 const sequelize = require('../db')
 const { DataTypes } = require('sequelize')
 
-Post.init({
- postId: DataTypes.STRING, 
- title: DataTypes.STRING,
- description: DataTypes.STRING,
- time: DataTypes.TIME
+const Post = pls.definePost(sequelize, {
+  postId: DataTypes.STRING,
+  title: DataTypes.STRING,
+  description: DataTypes.STRING,
+  time: DataTypes.TIME
 })
 
 module.exports = Post
