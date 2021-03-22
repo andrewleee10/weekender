058e92ce1b624e253d1ffdffe314e4cba84b54c6
Smeza314 <sebastianmeza314@gmail.com>
2021-03-22 11:35:35 -0700

    Took out username

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const { DataTypes } = require('sequelize')
 
 const User = pls.defineUser(sequelize, {
   name: DataTypes.STRING,
-  username: DataTypes.STRING,
   email: DataTypes.STRING,
   password: DataTypes.STRING,
   phone: DataTypes.STRING
