8a5c6b0fadb762f001b3c5c54f01ba5b26a1de54
Smeza314 <sebastianmeza314@gmail.com>
2021-03-24 11:43:59 -0700

    a

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,35 +3,34 @@ const { Post } = require('../models')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 
-router.get('/posts', passport.authenticate('jwt'), (req, res) => {
-  Post.findAll({ 
-    where: { uid: req.user.id } 
+router.get('/posts', (req, res) => {
+  Post.findAll({
+
   })
     .then(posts => res.json(posts))
     .catch(err => console.log(err))
 })
 
-router.get('/posts/:id', passport.authenticate('jwt'), (req, res) => {
-  Post.findOne({ 
-    where: { id: req.params.id } 
+router.get('/posts/:id', (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id }
   })
-    .then(post => res.json(item))
+    .then(post => res.json(post))
     .catch(err => console.log(err))
 })
 
 router.post('/posts', passport.authenticate('jwt'), (req, res) => {
   Post.create({
     title: req.body.title,
-    description: req.body.description,
-    uid: req.user.id
+    description: req.body.description
   })
-  .then(post => res.json(post))
-  .catch(err => console.log(err))
+    .then(post => res.json(post))
+    .catch(err => console.log(err))
 })
 
 router.put('/posts/:id', passport.authenticate('jwt'), (req, res) => {
-  Post.update(req.body, { 
-    where: {id: req.params.id} 
+  Post.update(req.body, {
+    where: { id: req.params.id }
   })
     .then(() => res.sendStatus(200))
     .catch(err => console.log(err))
