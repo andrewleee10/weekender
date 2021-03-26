01789821fd187e313195337f8df62e9f40e6812e
Andrew Lee <andrewleee10@gmail.com>
2021-03-26 11:38:22 -0700

    code fixes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
+router.use(require('./viewRoutes.js'))
 router.use('/api', require('./commentRoutes.js'))
 router.use('/api', require('./postRoutes.js'))
 router.use('/api', require('./userRoutes.js'))
